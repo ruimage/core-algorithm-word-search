3338f7e8bb98ecd0506d2cdeea4480363b0701b7
Anton Valkovsky <ruizave@gmail.com>
2022-05-19 15:43:28 +0300

    реализованы тесты

--- spec/wordSearch.spec.js
+++ spec/wordSearch.spec.js
@@ -1,24 +1,78 @@
 const data = require('../data/data');
+const {
+  searchStraightLineInclude,
+  searchSnakingInclude,
+  getRSymbol,
+  getLSymbol,
+  getUSymbol,
+  getURSymbol,
+  getDRSymbol, getDSymbol, getULSymbol, getDLSymbol,
+} = require('../wordSearch');
 
-describe ('searchStraightLineInclude', () => {
+describe('проверка функциоаналп получения соседнего символа', () => {
+  const { puzzle } = data;
 
-  it(, () => {
-    expect()
-  })
+  it('должно получить символ справа от текущего в массиве', () => {
+    expect(getRSymbol(puzzle, 2, 1)).toBe('w');
+    expect(getRSymbol(puzzle, 6, 0)).toBe(null);
+  });
 
-  it('', () => {
-    expect()
-  })
-})
+  it('должно получить символ справа вверх от текущего в массиве', () => {
+    expect(getURSymbol(puzzle, 2, 1)).toBe('o');
+    expect(getURSymbol(puzzle, 6, 0)).toBe(null);
+  });
 
+  it('должно получить символ справа вниз от текущего в массиве', () => {
+    expect(getDRSymbol(puzzle, 2, 1)).toBe('k');
+    expect(getDRSymbol(puzzle, 6, 0)).toBe(null);
+  });
 
-describe ('searchSnakingInclude', () => {
-  it('', () => {
-    expect()
-  })
+  it('должно получить символ слева от текущего в массиве', () => {
+    expect(getLSymbol(puzzle, 2, 1)).toBe('o');
+    expect(getLSymbol(puzzle, 0, 1)).toBe(null);
+  });
 
-  it('', () => {
-    expect()
-  })
-})
+  it('должно получить символ слева вверх от текущего в массиве', () => {
+    expect(getULSymbol(puzzle, 2, 1)).toBe('k');
+    expect(getULSymbol(puzzle, 0, 1)).toBe(null);
+  });
 
+  it('должно получить символ слева вниз от текущего в массиве', () => {
+    expect(getDLSymbol(puzzle, 2, 1)).toBe('t');
+    expect(getDLSymbol(puzzle, 0, 1)).toBe(null);
+  });
+
+  it('должно получить символ сверху от текущего в массиве', () => {
+    expect(getUSymbol(puzzle, 2, 1)).toBe('f');
+    expect(getUSymbol(puzzle, 2, 0)).toBe(null);
+  });
+
+  it('должно получить символ снизу от текущего в массиве', () => {
+    expect(getDSymbol(puzzle, 2, 1)).toBe('c');
+    expect(getDSymbol(puzzle, 2, 6)).toBe(null);
+  });
+});
+
+describe('searchStraightLineInclude', () => {
+  it('проверка нахождения слова foxes', () => {
+    expect(searchStraightLineInclude(data.wordsStraight[0], data.puzzle)).toBe(true);
+  });
+
+  it('проверка нахождения слова otters', () => {
+    expect(searchStraightLineInclude(data.wordsStraight[1], data.puzzle)).toBe(true);
+  });
+
+  it('проверка нахождения слова bison', () => {
+    expect(searchStraightLineInclude(data.wordsStraight[2], data.puzzle)).toBe(true);
+  });
+
+  it('проверка нахождения слова crown', () => {
+    expect(searchStraightLineInclude(data.wordsStraight[2], data.puzzle)).toBe(false);
+  });
+});
+
+describe('searchSnakingInclude', () => {
+  it('проверка нахождения слова nighthawks', () => {
+    expect(searchSnakingInclude(data.wordsSnake[0], data.puzzle)).toBe(true);
+  });
+});
--- wordSearch.js
+++ wordSearch.js
@@ -1,7 +1,51 @@
-function searchStraightLineInclude(word, puzzle){
+function searchStraightLineInclude(word, puzzle) {
 
 }
 
-function searchSnakingInclude(word, puzzle){
+function searchSnakingInclude(word, puzzle) {
 
 }
+
+const getRSymbol = (puzzle, i, j) => {
+
+};
+const getLSymbol = (puzzle, i, j) => {
+
+};
+
+const getUSymbol = (puzzle, i, j) => {
+
+};
+
+const getDSymbol = (puzzle, i, j) => {
+
+};
+
+const getURSymbol = (puzzle, i, j) => {
+
+};
+
+const getULSymbol = (puzzle, i, j) => {
+
+};
+
+const getDRSymbol = (puzzle, i, j) => {
+
+};
+
+const getDLSymbol = (puzzle, i, j) => {
+
+};
+
+module.exports = {
+  searchSnakingInclude,
+  searchStraightLineInclude,
+  getRSymbol,
+  getLSymbol,
+  getUSymbol,
+  getDSymbol,
+  getURSymbol,
+  getULSymbol,
+  getDRSymbol,
+  getDLSymbol,
+};
